37151dfdaf89a0207904fe243c4ee2ad63fd955e
Stun4Life <weeweiren@gmail.com>
2021-05-04 15:38:40 +0800

    Initial commit

---
+++ app/src/main/java/sg/edu/rp/c346/id19021138/demodatabase/TaskAdapter.java
@@ -0,0 +1,15 @@
+package sg.edu.rp.c346.id19021138.demodatabase;
+
+import android.content.Context;
+import android.widget.ArrayAdapter;
+
+import java.util.ArrayList;
+
+public class TaskAdapter extends ArrayAdapter<Task> {
+
+    private ArrayList<Task> taskArrayList;
+    Context context;
+
+    
+
+}
